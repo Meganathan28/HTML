bf962f7d108069cbba90d2205a67ea83555aa521
Meganathan Saravanan <meganathansaravanan4615@gmail.com>
2025-04-14 12:59:29 +0530

    Update

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+const myheading = document.querySelector("h3");
+myheading.textContent = "Luffy";
+
+document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+  });
---
+++ scripts/test.js
@@ -0,0 +1,9 @@
+function add(num, num1){
+    return num+num1;
+}
+
+let out  = add(1,2);
+console.log(out);
+
+out = add(3,4);
+console.log(out);
