cf81e9476fba1e97e6bde27cd26ef2ed4447624e
Meganathan Saravanan <meganathansaravanan4615@gmail.com>
2025-04-15 07:58:18 +0530

    Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,25 @@
-const myheading = document.querySelector("h3");
-myheading.textContent = "Luffy";
+const myButton = document.querySelector("button");
+const myHeading = document.querySelector("h1");
 
-document.querySelector("html").addEventListener("click", function () {
-    alert("Ouch! Stop poking me!");
-  });
+myButton.addEventListener("click",()=>{
+  setUserName();
+});
+
+function setUserName(){
+  const userName = prompt("Enter your Name:");
+  if(!userName){
+    setUserName();
+  }
+  else{
+    localStorage.setItem("name",userName);
+    myHeading.textContent = `Welcome, ${userName}`;
+  }
+}
+
+if(!localStorage.getItem("name")){
+  setUserName();
+}
+else{
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Welcome, ${storedName}`;
+}
